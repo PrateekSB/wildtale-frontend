b6d902c8134128b13a7bbe1b1d19fd6599e89264
Akash-M <akash.manjunath@gmail.com>
2019-11-16 23:10:23 +0100

    Update PostStory Layout

--- src/views/PostStory.vue
+++ src/views/PostStory.vue
@@ -7,38 +7,41 @@
         </div>
       </section>
     </article>
-      <section class="timeline">
-        <vue-timeline-update
-          description="Each story can consist a maximum
-          f 3 chapters                ."
-          thumbnail=""
-          category="Step 1"
-          icon="vue"
-          color="green"
-        />
+    <section class="timeline">
+      <vue-timeline-update
+        description="Each story can consist a maximum
+        f 3 chapters                ."
+        thumbnail=""
+        category="Step 1"
+        icon="vue"
+        color="orange"
+      />
 
-        <vue-timeline-update
-          description="You can upload images and video for &nbsp; each chapter."
-          thumbnail=""
-          category="Step 2"
-          icon="js"
-          color="purple"
-        />
+      <vue-timeline-update
+        description="You can upload images and video for &nbsp; each chapter."
+        thumbnail=""
+        category="Step 2"
+        icon="js"
+        color="purple"
+      />
 
-        <vue-timeline-update
-          description="After the final chapter,
-          &nbsp; submit story with author name!"
-          thumbnail=""
-          category="Step 3"
-          icon="css"
-          color="orange"
-          is-last
-        />
+      <vue-timeline-update
+        description="After the final chapter,
+        &nbsp; submit story with author name!"
+        thumbnail=""
+        category="Step 3"
+        icon="css"
+        color="green"
+        is-last
+      />
+      <section class="button_container">
+        <div class="box">
+          <router-link to="/create-story" class="btn btn-white btn-animation-1">
+            Create a story
+            <font-awesome-icon :icon="['fas', 'angle-right']" class="icon"/>
+          </router-link>
+        </div>
       </section>
-    <section class="button_container">
-      <div class="box">
-        <router-link to="/create-story" class="btn btn-white btn-animation-1">Create a story -></router-link>
-      </div>
     </section>
   </article>
 </template>
@@ -51,84 +54,88 @@ import Navbar from '@/components/Navbar.vue';
 export default class PostStory extends Vue { }
 </script>
 
+
 <style lang="scss" scoped>
-.post-story {
-  display: flex;
-  flex-direction: column;
-  .header {
-    height: 8vh;
-    @include desktop {
-      height: 6vh;
-    }
-    padding: 0.5rem;
+  .post-story {
     display: flex;
-    align-content: flex-start;
-    justify-content: left;
-    border-bottom: $stack-wildlife-border;
-    box-shadow: $stack-wildlife-shadow-box;
-    .title {
-      text-align: center;
-      font-weight: bold;
-      font-style: normal;
-      height: 5vh;
-      /* GLOBAL STYLES */
-      /* DEMO-SPECIFIC STYLES */
-      .typewriter h1 {
-        color: rgba(30, 7, 66, 0.65);
-        font-family: Lato, sans-serif;
-        overflow: hidden; /* Ensures the content is not revealed until the animation */
-        border-right: .15em solid black; /* The typwriter cursor */
-        white-space: nowrap; /* Keeps the content on a single line */
-        margin: 0 auto; /* Gives that scrolling effect as the typing happens */
-        letter-spacing: .15em; /* Adjust as needed */
-        animation:
-          typing 3.5s steps(30, end),
-          blink-caret .5s step-end infinite;
+    flex-direction: column;
+    .header {
+      height: 8vh;
+      @include desktop {
+        height: 6vh;
       }
-
-      /* The typing effect */
-      @keyframes typing {
-        from { width: 0 }
-        to { width: 100% }
-      }
-
-      /* The typewriter cursor effect */
-      @keyframes blink-caret {
-        from, to { border-color: transparent }
-        50% { border-color: orange }
-      }
-    }
-    .text {
+      padding: 0.5rem;
       display: flex;
-      flex-direction: row;
-      height: 10vh;
-      .number-circle {
-        border-radius: 50px;
-        width: 40px;
-        height: 40px;
-        padding: 0.5rem;
-        background-image: linear-gradient($stack-wildlife-green-light, $stack-wildlife-green);
-        color: white;
+      align-content: flex-start;
+      justify-content: left;
+      border-bottom: $stack-wildlife-border;
+      box-shadow: $stack-wildlife-shadow-box;
+      .title {
         text-align: center;
-        display: inline-block;
+        margin: auto;
+        font-weight: bold;
+        font-style: normal;
+        height: 5vh;
+        /* GLOBAL STYLES */
+        /* DEMO-SPECIFIC STYLES */
+        .typewriter h1 {
+          color: rgba(30, 7, 66, 0.65);
+          font-family: Lato, sans-serif;
+          overflow: hidden; /* Ensures the content is not revealed until the animation */
+          border-right: .15em solid black; /* The typwriter cursor */
+          white-space: nowrap; /* Keeps the content on a single line */
+          margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+          letter-spacing: .15em; /* Adjust as needed */
+          animation:
+            typing 3.5s steps(30, end),
+            blink-caret .5s step-end infinite;
+        }
+
+        /* The typing effect */
+        @keyframes typing {
+          from { width: 0 }
+          to { width: 100% }
+        }
+
+        /* The typewriter cursor effect */
+        @keyframes blink-caret {
+          from, to { border-color: transparent }
+          50% { border-color: orange }
+        }
+      }
+      .text {
+        display: flex;
+        flex-direction: row;
+        height: 10vh;
+        .number-circle {
+          border-radius: 50px;
+          width: 40px;
+          height: 40px;
+          padding: 0.5rem;
+          background-image: linear-gradient($stack-wildlife-green-light, $stack-wildlife-green);
+          color: white;
+          text-align: center;
+          display: inline-block;
+        }
       }
     }
-    .create {
-      display: flex;
-      justify-content: center;
-      align-content: center;
+    .timeline {
+      margin: 2rem auto auto;
+      /deep/ .c-vue-timeline-update__ago {
+        font-size: 0px;
+      }
+      /deep/ .c-vue-timeline-update__description {
+        color: rgba(30, 7, 66, 0.65);
+        font-size: 18px;
+      }
+      /deep/ .c-vue-timeline-update__category {
+        font-family: Lato, sans-serif;
+      }
+    }
+    .icon {
+      font-size: 1.2rem;
+      margin-left: 0.5rem;
+      color: $stack-wildlife-green;
     }
   }
-  .timeline {
-    justify-content: center;
-    align-items: center;
-    /deep/ .c-vue-timeline-update__ago {
-      font-size: 0px;
-    }
-    /deep/ .c-vue-timeline-update__description {
-      color: rgba(30, 7, 66, 0.65);
-      font-size: 18px;
-    }
-  }
-}
 </style>
